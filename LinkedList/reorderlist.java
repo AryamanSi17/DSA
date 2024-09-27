6cc6c347b053a8486dcd60329a079c53ac070aab
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-09-27 19:29:57 +0530

    pushed problem on linked list

---
+++ LinkedList/reorderlist.java
@@ -0,0 +1,34 @@
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int val){
+        this.val=val;
+        this.next=null;
+    }
+}
+public class reorderlist {
+    public void reorderList(ListNode head) {
+        if(head==null || head.next==null) return;
+        ListNode slow=head,fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode prev=null,curr=slow;
+        while(curr!=null){
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
+        }
+        ListNode first = head, second = prev;
+        while(second.next!=null){
+            ListNode temp1=first.next;
+            ListNode temp2=second.next;
+            first.next=second;
+            second.next=temp1;
+            first=temp1;
+            second=temp2;
+        }
+    }
+}

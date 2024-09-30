e976f018ea134747cc55175879d978610da8ff48
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-09-30 20:33:45 +0530

    pushed linked list problem

--- LinkedList/reversedoublylist.java
+++ LinkedList/reversedoublylist.java
@@ -1,15 +1,16 @@
 public class reversedoublylist {
-    public Node reverse(Node head){
-        if(head==null || head.next ==null) return head;
-        Node prev=null;
-        Node curr=head;
-        Node next=null;
-        while(curr!=null){
-            next=curr.next;
-            curr.next=prev;
-            curr.prev=next;
-            prev=curr;
-            curr=next;
+    public Node reverse(Node head) {
+        if (head == null || head.next == null)
+            return head;
+        Node prev = null;
+        Node curr = head;
+        Node next = null;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            curr.prev = next;
+            prev = curr;
+            curr = next;
         }
         return prev;
     }

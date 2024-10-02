fcf75fa163164fdaf92054f09f294d4e43187ba6
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-10-02 17:32:37 +0530

    pushed

--- LinkedList/reversedoublylist.java
+++ LinkedList/reversedoublylist.java
@@ -1,3 +1,13 @@
+class Node{
+    int data;
+    Node next;
+    Node prev;
+    Node(int data){
+        this.data=data;
+        this.next=null;
+        this.prev=null;
+    }
+}
 public class reversedoublylist {
     public Node reverse(Node head) {
         if (head == null || head.next == null)

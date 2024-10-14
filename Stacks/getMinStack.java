678583d581296ca2eb9cb6b710af1486027c9c0f
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-10-14 23:21:56 +0530

    pushed stack problem

---
+++ Stacks/getMinStack.java
@@ -0,0 +1,33 @@
+class MinStack {
+    private Deque<Integer> s;
+    private Deque<Integer> minStack;
+
+    public MinStack() {
+        s = new ArrayDeque<>();
+        minStack = new ArrayDeque<>();
+    }
+
+    public void push(int val) {
+        s.push(val);
+        // If minStack is empty or the new value is less than or equal to the current minimum
+        if (minStack.isEmpty() || val <= minStack.peek()) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int removed = s.pop();
+        // If the removed value is the current minimum, pop it from minStack too
+        if (removed == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return s.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}

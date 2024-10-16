53d14e2bae25a52972e2f2b780f5504e6fe3b18a
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-10-17 00:26:40 +0530

    pushed trees problem

---
+++ Trees/maxDepthofBinaryTree.java
@@ -0,0 +1,12 @@
+package Trees;
+
+public class maxDepthofBinaryTree {
+    int height(Node root){
+        if(root==null){
+            return 0;
+        }
+        int left=height(root.left);
+        int right=height(root.right);
+        return Math.max(left,right)+1;
+    }
+}
---
+++ Trees/nodesAtDistanceK.java
@@ -0,0 +1,13 @@
+package Trees;
+
+public class nodesAtDistanceK {
+    void printKDist(Node root,int k){
+        if(root==null) return;
+        if(k==0){
+            System.out.print(root.data+" ");
+            return;
+        }
+        printKDist(root.left,k-1);
+        printKDist(root.right,k-1);
+    }
+}

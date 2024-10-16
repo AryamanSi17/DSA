1e2eb28db16b86c6497d592d945df700628c1b6e
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-10-16 23:23:24 +0530

    pushed trees traversal problem code

---
+++ Trees/inOrderTraversal.java
@@ -0,0 +1,20 @@
+package Trees;
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+public class inOrderTraversal {
+    void inOrder(Node root){
+        if(root!=null){
+            inOrder(root.left);
+            System.out.print(root.data+" ");
+            inOrder(root.right);
+        }
+    }
+}
---
+++ Trees/preOrderTraversal.java
@@ -0,0 +1,11 @@
+package Trees;
+
+public class preOrderTraversal {
+    void preorder(Node root) {
+        if (root != null) {
+            System.out.print(root.data + " ");
+            preorder(root.left);
+            preorder(root.right);
+        }
+    }
+}

dd9befb493e8c43a7a2b9f7093d87be82bde660e
Aryaman Sinha <sinhasanjeevkumar08@gmail.com>
2024-10-20 15:54:33 +0530

    pushed Trees problem

---
+++ Trees/diameterOfBinaryTree.java
@@ -0,0 +1,19 @@
+package Trees;
+
+public class diameterOfBinaryTree {
+    private int res = 0;
+
+    public int calculatediameter(Node root) {
+        height(root);
+        return res;
+    }
+
+    public int height(Node root) {
+        if (root == null)
+            return 0;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        res = Math.max(res, lh + rh);
+        return 1 + Math.max(lh, rh);
+    }
+}
